4e689081e6d9b49df4759dd108a234d71eb4194c
kokororin <i@kotori.love>
2018-01-04 10:48:17 +0800

    Use `chai-as-promised` to test Promise rejection

--- test/generatorTest.ts
+++ test/generatorTest.ts
@@ -1,15 +1,20 @@
 ///<reference path="../node_modules/@types/node/index.d.ts"/>
 ///<reference path="../node_modules/@types/mocha/index.d.ts"/>
-///<reference path="../node_modules/@types/power-assert/index.d.ts"/>
+///<reference path="../node_modules/@types/chai/index.d.ts"/>
 ///<reference path="../node_modules/@types/nock/index.d.ts"/>
 
-import * as assert from 'power-assert';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
 import * as cp from 'child_process';
 import { URL } from 'url';
 import * as path from 'path';
 import honoka from 'honoka';
 import * as nock from 'nock';
 import generator = require('../src/generator');
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
 
 const BIN_PATH = `node ${path.join(__dirname, '/../bin/muse.js')}`;
 const ORIGINAL_REGEX = /music.126.net/;
@@ -34,43 +39,30 @@ describe('generator function test', () => {
     nock.cleanAll();
   });
 
-  it('input a non-numberic value should throw error', async () => {
-    let err: any;
-    try {
-      await generator('niconiconi');
-    } catch (e) {
-      err = e;
-    }
-    assert.throws(() => {
-      throw err;
-    }, /invalid song ID/);
+  it('input a non-numberic value should throw error', () => {
+    expect(generator('niconiconi')).to.be.rejectedWith(/invalid song ID/);
   });
 
   it('input two song ID should output an array contained two objects', async () => {
     let playlist: any = await generator(TEST_SONGS[0], TEST_SONGS[1]);
     playlist = JSON.parse(playlist);
-    assert.equal(true, Array.isArray(playlist));
-    assert.equal(2, playlist.length);
+    expect(Array.isArray(playlist)).to.be.true;
+    expect(playlist.length).to.equal(2);
   });
 
   it('output should contain specified keys', async () => {
     let playlist: any = await generator(TEST_SONGS[0]);
     playlist = JSON.parse(playlist);
     PLAYLIST_KEYS.forEach(key => {
-      assert.equal(
-        true,
-        Object.prototype.hasOwnProperty.call(playlist[0], key)
-      );
+      expect(Object.prototype.hasOwnProperty.call(playlist[0], key)).to.be.true;
     });
   });
 
   it('artists should be divided with / when a song has more than one artist', async () => {
     let playlist: any = await generator(TEST_SONGS[2]);
     playlist = JSON.parse(playlist);
-    assert.equal(
-      true,
-      String.prototype.indexOf.call(playlist[0].artist, '/') > -1
-    );
+    expect(String.prototype.indexOf.call(playlist[0].artist, '/') > -1).to.be
+      .true;
   });
 
   it('src should contain temporary link when user apply temporary mode', async () => {
@@ -80,7 +72,7 @@ describe('generator function test', () => {
     playlist = JSON.parse(playlist);
     const src = playlist[0].src;
 
-    assert.equal(true, ORIGINAL_REGEX.test(src));
+    expect(ORIGINAL_REGEX.test(src)).to.be.true;
 
     const srcURL = new URL(src);
 
@@ -88,73 +80,49 @@ describe('generator function test', () => {
       await honoka.head(
         `https://api.kotori.love/netease/${srcURL.host}${srcURL.pathname}`
       );
-      assert.equal(200, honoka.response.status);
-      assert.equal('audio/mpeg', honoka.response.headers.get('Content-Type'));
+      expect(honoka.response.status).to.equal(200);
+      expect(honoka.response.headers.get('Content-Type')).to.equal(
+        'audio/mpeg'
+      );
     }
   });
 
   it('lyric should be undefined when a song has not lyric', async () => {
     let playlist: any = await generator(TEST_SONGS[4]);
     playlist = JSON.parse(playlist);
-    assert.equal('undefined', typeof playlist[0].lyric);
+    expect(typeof playlist[0].lyric).to.be.equal('undefined');
   });
 
-  it('should throw error when input is empty', async () => {
-    let err: any;
-    try {
-      await generator();
-    } catch (e) {
-      err = e;
-    }
-    assert.throws(() => {
-      throw err;
-    }, /invalid input/);
+  it('should throw error when input is empty', () => {
+    expect(generator()).to.be.rejectedWith(/invalid input/);
   });
 
-  it('should throw error when song is not found', async () => {
-    let err: any;
-    try {
-      await generator(TEST_SONGS[3]);
-    } catch (e) {
-      err = e;
-    }
-    assert.throws(() => {
-      throw err;
-    }, `song ID ${TEST_SONGS[3]} is not exist`);
+  it('should throw error when song is not found', () => {
+    expect(generator(TEST_SONGS[3])).to.be.rejectedWith(
+      `song ID ${TEST_SONGS[3]} is not exist`
+    );
   });
 
-  it('should throw error when response is not valid', async () => {
+  it('should throw error when response is not valid', () => {
     nock(/(.*)/)
       .get(/(.*)/)
       .query(() => true)
       .reply(200, '{');
 
-    let err: any;
-    try {
-      await generator(TEST_SONGS[0]);
-    } catch (e) {
-      err = e;
-    }
-    assert.throws(() => {
-      throw err;
-    }, /response is not a valid json string/);
+    expect(generator(TEST_SONGS[0])).to.be.rejectedWith(
+      /response is not a valid json string/
+    );
   });
 
-  it("should throw error when response's code is not 200", async () => {
+  it("should throw error when response's code is not 200", () => {
     nock(/(.*)/)
       .get(/(.*)/)
       .query(() => true)
       .reply(200, '{"code":400}');
 
-    let err: any;
-    try {
-      await generator(TEST_SONGS[0]);
-    } catch (e) {
-      err = e;
-    }
-    assert.throws(() => {
-      throw err;
-    }, /response code is not valid/);
+    expect(generator(TEST_SONGS[0])).to.be.rejectedWith(
+      /response code is not valid/
+    );
   });
 });
 
@@ -164,7 +132,7 @@ describe('generator cli test', () => {
       .execSync(`${BIN_PATH} ${TEST_SONGS[0]} --stdout`)
       .toString();
     playlist = JSON.parse(playlist);
-    assert.equal(1, playlist.length);
+    expect(playlist.length).to.equal(1);
   });
 
   it('stdout mode should work with two song ID', () => {
@@ -172,7 +140,7 @@ describe('generator cli test', () => {
       .execSync(`${BIN_PATH} ${TEST_SONGS[0]} ${TEST_SONGS[1]} --stdout`)
       .toString();
     playlist = JSON.parse(playlist);
-    assert.equal(2, playlist.length);
+    expect(playlist.length).to.equal(2);
   });
 
   it('temporary mode should work', () => {
@@ -180,7 +148,7 @@ describe('generator cli test', () => {
       `${BIN_PATH} ${TEST_SONGS[0]} --temporary --stdout`
     );
     playlist = JSON.parse(playlist);
-    assert.equal(1, playlist.length);
-    assert.equal(true, ORIGINAL_REGEX.test(playlist[0].src));
+    expect(playlist.length).to.equal(1);
+    expect(ORIGINAL_REGEX.test(playlist[0].src)).to.be.true;
   });
 });
